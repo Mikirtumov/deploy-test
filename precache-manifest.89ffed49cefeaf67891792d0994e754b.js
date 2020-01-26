ae17397b8c93e14f4638f0c4e7d4bcd0dacb4f3a
Mikirtumov <you@example.com>
2020-01-25 23:01:21 -0500

    Updates

---
+++ precache-manifest.89ffed49cefeaf67891792d0994e754b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb1989c9366005abb6475c3ede56817",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "e9c70cd1036b629fe7bd",
+    "url": "/deploy-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d912a6a5b1030259b0d",
+    "url": "/deploy-test/static/js/2.024362da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-test/static/js/2.024362da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9c70cd1036b629fe7bd",
+    "url": "/deploy-test/static/js/main.5c0d67e0.chunk.js"
+  },
+  {
+    "revision": "86240cf438f5d4c41fd8",
+    "url": "/deploy-test/static/js/runtime-main.809d3971.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
